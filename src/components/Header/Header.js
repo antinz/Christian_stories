d40cb31bdc2bb967bf2e569a894d0a6d311d740e
antinz <antoninzhirov@gmail.com>
2023-10-02 15:12:59 +0300

    responsive design and dev

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import classes from "./Header.module.css";
 import SelectorForm from "./SelectorForm";
-
+import AboutAuthorButton from "./AboutAuthorButton";
+import { FaBars, FaTimes } from "react-icons/fa";
 const Header = ({
   handleSelectChange,
   onAboutAuthorClick,
   showAboutAuthor,
 }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [showSelectorForm, setShowSelectorForm] = useState(true);
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle(classes["responsive-nav"]);
+    setShowSelectorForm(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <header className={classes.header}>
+      <button onClick={showNavBar} className={classes["nav-btn"]}>
+        <FaBars />
+      </button>
       <h1>Христианские рассказы</h1>
-      <nav className={classes["header-menu"]}>
-        {!showAboutAuthor && (
-          <SelectorForm handleSelectChange={handleSelectChange} />
-        )}
-
-        {!showAboutAuthor && (
-          <button onClick={onAboutAuthorClick}>Об авторе</button>
-        )}
+      {!showAboutAuthor && (
+        <SelectorForm handleSelectChange={handleSelectChange} />
+      )}
+      <nav ref={navRef} className={classes["header-menu"]}>
+        <div className={classes["header-btn"]}>
+          {!showAboutAuthor && (
+            <AboutAuthorButton onAboutAuthorClick={onAboutAuthorClick} />
+          )}
+        </div>
+        <button
+          onClick={showNavBar}
+          className={`${classes["nav-btn"]} ${classes["nav-close-btn"]}`}
+        >
+          <FaTimes />
+        </button>
       </nav>
     </header>
   );
